582215f855fa21e9bfe7f5007abc6b160a102f1a
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-12 10:08:31 -0700

    added within block

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -18,16 +18,18 @@ RSpec.describe "Merchant Bulk Discounts Index" do
 
         expect(page).to have_link("View all my discounts")
         click_link("View all my discounts")
-
+        
         expect(current_path).to eq "/merchants/#{@merchant1.id}/bulk_discounts"
-
-        expect(page).to have_link(@discount_a.name)
-        expect(page).to have_content(@discount_a.percentage_discount)
-        expect(page).to have_content(@discount_a.quantity_threshold)
-
-        expect(page).to have_link(@discount_b.name)
-        expect(page).to have_content(@discount_b.percentage_discount)
-        expect(page).to have_content(@discount_b.quantity_threshold)
+        
+        within("#discounts-index") do
+          expect(page).to have_link(@discount_a.name)
+          expect(page).to have_content(@discount_a.percentage_discount)
+          expect(page).to have_content(@discount_a.quantity_threshold)
+          
+          expect(page).to have_link(@discount_b.name)
+          expect(page).to have_content(@discount_b.percentage_discount)
+          expect(page).to have_content(@discount_b.quantity_threshold)
+        end
 
         click_link(@discount_a.name)
 
